e7578d6917d24d74b617a7472f621f92038d69b3
Mahmoud Wael <Mahmoud Wael@DESKTOP-UTQ37J8>
2024-10-05 23:23:28 +0300

    Add project files.

---
+++ Handlig-Vowels/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Handlig_Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            
+                try
+                {
+                   
+                    Console.WriteLine("please Enter a string:");
+                    string input = Console.ReadLine();
+
+                   
+                    CheckForVowels(input);
+
+                  
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            
+            static void CheckForVowels(string input)
+            {
+                
+                input = input.ToLower();
+
+                
+                bool hasVowel = false;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char c = input[i];
+
+                    
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        hasVowel = true;
+                        Console.WriteLine(" string contains vowels.");
+                        break;
+                       
+                }
+                }
+
+               
+                if (!hasVowel)
+                {
+                    throw new Exception(" not contain any vowels.");
+                }
+            }
+        }
+    }
+    
+
